311c750e2a8ba9ece5377b3e5f830a86431ddc89
Henry Chambi <henrychambi39@gmail.com>
2025-05-17 14:14:13 -0400

    Sesion 04

---
+++ sesion04.py
@@ -0,0 +1,12 @@
+# Example of execution flow
+# Instruction 1
+print("START OF THE PROGRAM")
+
+# Instruction 2
+print("PRINT SECOND INSTRUCTION")
+
+# Instruction 3
+print("HELLO, WORLD")
+
+# Instruction 4
+print("END OF THE PROGRAM")
