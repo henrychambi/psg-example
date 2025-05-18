1acba27104f5d18afa8f500483cf41bbbc1d5589
Henry Chambi <henrychambi39@gmail.com>
2025-05-17 21:33:03 -0400

    ejercicio 03

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,8 @@
+# CODE 3 - ejercicio 3
+print ("    REGLA DE JUEGO SNACK AND LADDER     ")
+print (" 1. Cada jugador elige una ficha y la coloca en la casilla de inicio (normalmente la casilla 1)")
+print (" 2. Los jugadores tiran el dado por turnos y avanzan su ficha la cantidad de casillas que indique el dado")
+print (" 3. Si una ficha cae en la cabeza de una serpiente, debe deslizarse hacia abajo hasta la cola de la serpiente")
+print (" 4. Si una ficha cae en la base de una escalera, debe subir hasta la parte superior de la escalera")
+print (" 5. El jugador que llega a la última casilla exactamente (sin superar la casilla final) gana el juego. Si un jugador saca un número que lo lleva a superar la casilla final, debe retroceder la cantidad de casillas excedidas")
+print (" 6. En algunos juegos, si un jugador saca un 6, puede volver a tirar el dado para un movimiento adicional, según algunas reglas")
